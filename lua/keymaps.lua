9d5f94e470e143062f31b511fa90340e98b06477
djfrmd <7611672+djfrmd@users.noreply.github.com>
2023-11-07 19:26:03 -0600

    Add trouble plugin

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -25,4 +25,13 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+-- [[ Configure Trouble ]]
+local trb = require('trouble')
+vim.keymap.set('n', '<leader>xx', function() trb.toggle() end, { desc = 'Toggle Trouble' })
+vim.keymap.set('n', '<leader>xw', function() trb.toggle('workspace_diagnostics') end, { desc = 'Workspace Diagnostics' })
+vim.keymap.set('n', '<leader>xd', function() trb.toggle('document_diagnostics') end, { desc = 'Document Diagnostics' })
+vim.keymap.set('n', '<leader>xq', function() trb.toggle('quickfix') end, { desc = 'Quickfix' })
+vim.keymap.set('n', '<leader>xl', function() trb.toggle('loclist') end, { desc = 'Location List' })
+vim.keymap.set('n', 'gR', function() trb.toggle("lsp_references") end, { desc = 'Lsp References' })
+
 -- vim: ts=2 sts=2 sw=2 et
