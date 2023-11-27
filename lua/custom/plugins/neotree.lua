956cc12a79a2462efe4d543e1096b5221fa776bd
djfrmd <7611672+djfrmd@users.noreply.github.com>
2023-11-27 07:39:50 -0600

    Add Neotree mappings

--- lua/custom/plugins/neotree.lua
+++ lua/custom/plugins/neotree.lua
@@ -6,6 +6,10 @@ return {
             'nvim-lua/plenary.nvim',
             'nvim-tree/nvim-web-devicons',
             'MunifTanjim/nui.nvim',
+        },
+        keys = {
+            { "<leader>nt", function() require("neo-tree.command").execute({toggle = true, source = "filesystem", position = "float",}) end, desc = "[n]eotree [f]loat" },
+            { "<leader>ns", function() require("neo-tree.command").execute({toggle = true, source = "filesystem", position = "left",}) end, desc = "[n]eotree [s]idebar" },
         }
     }
 }
