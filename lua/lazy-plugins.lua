de13a26be9496a6616e0acb66b72c64b83c25cd7
djfrmd <7611672+djfrmd@users.noreply.github.com>
2024-03-26 23:14:44 -0500

    Add custom mappings

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -64,10 +64,12 @@ require('lazy').setup({
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
       },
+      numhl = true,
       on_attach = function(bufnr)
-        vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'GitSigns [H]unk [P]review' })
-        vim.keymap.set('n', '<leader>hs', require('gitsigns').stage_hunk, { buffer = bufnr, desc = 'GitSigns [H]unk [S]tage' })
-        vim.keymap.set('n', '<leader>hu', require('gitsigns').reset_hunk, { buffer = bufnr, desc = 'GitSigns [H]unk [U]ndo' })
+        vim.keymap.set('n', '<leader>gp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[G]it hunk [P]review' })
+        vim.keymap.set('n', '<leader>gs', require('gitsigns').stage_hunk, { buffer = bufnr, desc = '[G]it hunk [S]tage' })
+        vim.keymap.set('n', '<leader>gu', require('gitsigns').reset_hunk, { buffer = bufnr, desc = '[G]it hunk [U]ndo' })
+        vim.keymap.set('n', '<leader>gb', require('gitsigns').blame_line, { buffer = bufnr, desc = '[G]it [B]lame line'})
 
         -- don't override the built-in and fugitive keymaps
         local gs = package.loaded.gitsigns
